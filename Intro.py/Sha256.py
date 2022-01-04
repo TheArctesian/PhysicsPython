47614bfa4061d5039019c495736d129794b2cec5
TheArctesian <stephen.d.okita@gmail.com>
2022-01-04 09:15:15 +0800

    sha256

--- Intro.py/Sha256.py
+++ Intro.py/Sha256.py
@@ -28,22 +28,20 @@ print (cl.HEADER + "____________________________________________________________
 print (cl.HEADER + "______________________________________________________________________")
 print (cl.HEADER + "______________________________________________________________________\n\n")
 print ("\r")
-  ```
-# initializing string
-str = "The Homie Hooper"
-result = hashlib.sha384(str.encode())
 
+
+""" 
+Other hash functions:
+
+result = hashlib.sha384(str.encode())
 print(cl.ENDC + "The hexadecimal equivalent of SHA384 is : ")
 print(result.hexdigest())
   
-# initializing string
-str = "The Homie Hooper"
-  
-# encoding GeeksforGeeks using encode()
-# then sending to SHA1()
+
+
 result = hashlib.sha1(str.encode())
-  
-# printing the equivalent hexadecimal value.
 print("The hexadecimal equivalent of SHA1 is : ")
 print(result.hexdigest())
-```
+
+
+"""
